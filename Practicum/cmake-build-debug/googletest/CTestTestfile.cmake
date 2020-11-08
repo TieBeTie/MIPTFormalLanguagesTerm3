95d4555a069f2ec9c5fa6b6dcd2fe259e7dbe2fd
Jerry Treeman <35148370+TieBeTie@users.noreply.github.com>
2020-11-08 20:31:09 +0600

    aa

--- Practicum/cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tiebetie/CLionProjects/Practicum/googletest
-# Build directory: /home/tiebetie/CLionProjects/Practicum/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
